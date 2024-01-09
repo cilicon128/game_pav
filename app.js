f15ad655848144613670d27421a78843cc03e4c4
Cilicon Ueki <79232217+cilicon128@users.noreply.github.com>
2024-01-09 10:27:15 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,40 +1,191 @@
 const quiz = [
     {
-        question: '事業とはどれ？',
+        question: 'CS(略称)の正解はどれ？　' + ' ⏩ 　' + ' ex.　' + ' ' + ' I _______ to fight.',
         answers: [
-            'project',
-            'program',
-            'plan',
-            'schedule'  
+            'employee satisfaction',
+            'customer satisfaction',
+            'consumer satisfaction',
+            'self satisfaction'  
         ],
-        correct: 'project'
+        correct: 'customer satisfaction'
     },
     {
-        question: 'ゲームソフト市場、最も売れたソフトは次の内どれ？',
+        question: '多様な　　' + ' ⏩ 　' + ' ex.　' + ' ' + ' There are _______ people in the USA.',
         answers: [
-            'インベーダー',
-            'ギャラクシカ',
-            'スーパーマリオ',
-            'パックマン'  
+            'divde',
+            'diverse',
+            'divorce',
+            'divine'  
         ],
-        correct: 'スーパーマリオ'
+        correct: 'diverse'
     },
     {
-        question: 'ノートPCで、最も売れたノートPCは次の内どれ？',
+        question: '拒否する　　' + ' ⏩ 　' + ' ex.　' + ' ' + ' We _______ their idea.',
         answers: [
-            'SONYVAIO',
-            'panasonicLetsnote',
-            'NECLAVIE',
-            'MacBookPro'  
+            'region',
+            'rejoice',
+            'regret',
+            'reject'  
         ],
-        correct: 'panasonicLetsnote'
+        correct: 'reject'  
+    },
+    {
+        question: '申し込む　　' + ' ⏩ 　' + ' ex.　' + ' ' + ' I _______ for the job.',
+        answers: [
+            'applied',
+            'application',
+            'applause',
+            'applicant'  
+        ],
+        correct: 'applied'  
+    },
+    {
+        question: '急がせる　　' + ' ⏩ 　' + ' ex.　' + ' ' + ' I ______ you to choose.',
+        answers: [
+            'urban',
+            'urine',
+            'urge',
+            'urn'  
+        ],
+        correct: 'urge'  
+    },
+    {
+        question: '後悔する　　' + ' ⏩ 　' + ' ex.　' + ' ' + ' I ______ I cannot help you',
+        answers: [
+            'regret',
+            'region',
+            'rejoice',
+            'reject'  
+        ],
+        correct: 'regret'
+    },
+    {
+        question: '本物の　　' + ' ⏩ 　' + ' ex.　' + ' ' + ' This is a ________ gold watch.',
+        answers: [
+            'author',
+            'authentic',
+            'authority',
+            'authenticity'  
+        ],
+        correct: 'authentic'
+    },
+    {
+        question: '禁止する　　' + ' ⏩ 　' + ' ex.　' + ' ' + ' They _______ smoking here.',
+        answers: [
+            'form',
+            'forward',
+            'forget',
+            'forbid'  
+        ],
+        correct: 'forbid'
+    },
+    {
+        question: '義務　　' + ' ⏩ 　' + ' ex.　' + ' ' + ' He has an _______ to the company.',
+        answers: [
+            'obligation',
+            'obivious',
+            'obtain',
+            'obvious'  
+        ],
+        correct: 'obligation'
+    },
+    {
+        question: '大切にする　' + ' ⏩ 　' + ' ex.　' + ' ' + ' I cherish his friendship.',
+        answers: [
+            'cherish',
+            'cheap',
+            'chest',
+            'cheerful'  
+        ],
+        correct: 'cherish'
+    },
+    {
+        question: '和解する　　' + ' ⏩ 　' + ' ex.　' + ' ' + ' We must ________ our defferences.',
+        answers: [
+            'recycle',
+            'recommend',
+            'reconcile',
+            'recruit'  
+        ],
+        correct: 'reconcile'
+    },
+    {
+        question: '控える　　' + ' ⏩ 　' + ' ex.　' + ' ' + ' Please _______ from smoking.',
+        answers: [
+            'refrain',
+            'refer',
+            'refund',
+            'refuse'  
+        ],
+        correct: 'refrain'  
+    },
+    {
+        question: '　　' + ' ⏩ 　' + ' ex.　' + ' ' + ' I _______ for the job.',
+        answers: [
+            'applide',
+            'application',
+            'applause',
+            'applicant'  
+        ],
+        correct: 'applied'  
+    },
+    {
+        question: '　　' + ' ⏩ 　' + ' ex.　' + ' ' + ' I ______ you to choose.',
+        answers: [
+            'urban',
+            'urine',
+            'urge',
+            'urn'  
+        ],
+        correct: 'urge'  
+    },
+    {
+        question: '　　' + ' ⏩ 　' + ' ex.　' + ' ' + ' I ______ I cannot help you',
+        answers: [
+            'regret',
+            'region',
+            'rejoice',
+            'reject'  
+        ],
+        correct: 'regret'
+    },
+    {
+        question: '　　' + ' ⏩ 　' + ' ex.　' + ' ' + ' This is a ________ gold watch.',
+        answers: [
+            'author',
+            'authentic',
+            'authority',
+            'authenticity'  
+        ],
+        correct: 'authentic'
+    },
+    {
+        question: '　　' + ' ⏩ 　' + ' ex.　' + ' ' + ' They _______ smoking here.',
+        answers: [
+            'form',
+            'forward',
+            'forget',
+            'forbid'  
+        ],
+        correct: 'forbid'
+    },
+    {
+        question: '　　' + ' ⏩ 　' + ' ex.　' + ' ' + ' He has an _______ to the company.',
+        answers: [
+            'obligation',
+            'obivious',
+            'obtain',
+            'obvious'  
+        ],
+        correct: 'obligation'
     }
+
 ];
 
 const quizLength = quiz.length;
 let quizIndex = 0;
 let score = 0;
-
+ 
 
 const $button = document.getElementsByTagName('button')
 const buttonLength = $button.length;
